5dc810330994845ebe6f807234bd2da944d1f749
Harshil Savaliya <142370919+22SOECE11052@users.noreply.github.com>
2025-12-28 10:16:53 +0530

    Add countNegatives method for LeetCode 1351
    Implement countNegatives method to count negative numbers in a sorted matrix.

--- python/1351_count_negatives.py/2444_most_booked.py
+++ python/1351_count_negatives.py/2444_most_booked.py
@@ -1,1 +1,19 @@
+# LeetCode 1351
+# https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+
+class Solution(object):
+    def countNegatives(self, grid):
+        m = len(grid)
+        n = len(grid[0])
+        row, col = 0, n - 1
+        count = 0
+
+        while row < m and col >= 0:
+            if grid[row][col] < 0:
+                count += (m - row)
+                col -= 1
+            else:
+                row += 1
+
+        return count
 
